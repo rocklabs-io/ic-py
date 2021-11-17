55cfafcf7171479ff12121cade4dbf22866ddcd9
ccyanxyz <ccyanxyz@gmail.com>
2021-11-18 00:03:58 +0800

    update principal

--- ic/principal.py
+++ ic/principal.py
@@ -1,2 +1,40 @@
 
 # principal type: https://github.com/dfinity/ic-types/blob/main/src/principal.rs
+
+from enum import Enum
+
+CRC_LENGTH_IN_BYTES = 4
+HASH_LENGTH_IN_BYTES = 28
+MAX_LENGTH_IN_BYTES = 29
+
+class PrincipalClass(Enum):
+    OpaqueId = 1
+    SelfAuthenticating = 2
+    DerivedId = 3
+    Anonymous = 4
+    Unassigned
+
+class Principal(Object):
+    def __init__(self):
+        self.len = 0
+        self.bytes = [0 * MAX_LENGTH_IN_BYTES]
+
+    @static
+    def management_canister():
+        pass
+
+    @static
+    def self_authenticating(pubkey):
+        pass
+
+    def from_str(): 
+        pass
+
+    def from_bytes():
+        pass
+
+    def to_bytes():
+        pass
+
+    def to_str():
+        pass
