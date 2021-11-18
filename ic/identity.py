84fb2bea10a836e818c6ae0f8a9363cf9803923f
ccyanxyz <ccyanxyz@gmail.com>
2021-11-18 23:35:47 +0800

    add ed25519

--- ic/identity.py
+++ ic/identity.py
@@ -3,34 +3,50 @@
 
 import ecdsa
 import hashlib
-from principal import Principal
+from . import der
+from .principal import Principal
 
-# TODO: add ed25519
+# TODO: complete ed25519 support
 class Identity:
-    def __init__(self, privkey = "", anonymous = False):
+    def __init__(self, privkey = "", type = "secp256k1", anonymous = False):
         self.anonymous = anonymous
         if anonymous:
             return
-        if len(privkey) > 0:
-            self._privkey = privkey
-            self.sk = ecdsa.SigningKey.from_string(privkey, curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
-            self.vk = self.sk.get_verifying_key()
-            self._pubkey = self.vk.to_string().hex()
-            self._der_pubkey = self.vk.to_der().hex()
-        else:
-            self.sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
+        self.key_type = type
+        if type == 'secp256k1':
+            if len(privkey) > 0:
+                self.sk = ecdsa.SigningKey.from_string(privkey, curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
+            else:
+                self.sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
             self._privkey = self.sk.to_string().hex()
             self.vk = self.sk.get_verifying_key()
             self._pubkey = self.vk.to_string().hex()
             self._der_pubkey = self.vk.to_der().hex()
+        elif type == 'ed25519':
+            if len(privkey) > 0:
+                self.sk = ed25519.SigningKey(privkey.encode(), encoding='hex') 
+                self.vk = self.sk.get_verifying_key() 
+            else:
+                (self.sk, self.vk) = ed25519.create_keypair()
+            self._privkey = self.sk.to_ascii(encoding='hex')
+            self._pubkey = self.vk.to_ascii(encoding='hex')
+            # self._der_pubkey = 
+        else:
+            raise 'unsupported identity type'
 
     @staticmethod
     def from_pem(pem: str):
-        sk = ecdsa.SigningKey.from_pem(pem)
-        return Identity(sk.to_string().hex())
+        if self.key_type == 'secp256k1':
+            sk = ecdsa.SigningKey.from_pem(pem)
+            return Identity(sk.to_string().hex())
+        else:
+            pass
 
     def to_pem(self):
-        return self.sk.to_pem()
+        if self.key_type == 'secp256k1':
+            return self.sk.to_pem()
+        else:
+            pass
 
     def sender(self):
         if self.anonymous:
@@ -52,7 +68,7 @@ class Identity:
         return self._pubkey
 
     def __repr__(self):
-        return "Identity(" + self._privkey + ", " + self._pubkey + ")"
+        return "Identity(" + self.key_type + ', ' + self._privkey + ", " + self._pubkey + ")"
 
     def __str__(self):
-        return "(" + self._privkey + ", " + self._pubkey + ")"
+        return "(" + self.key_type + ', ' self._privkey + ", " + self._pubkey + ")"
