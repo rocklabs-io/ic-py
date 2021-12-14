e739dccfdeba46e6116555a5ffae5340d4b1942e
mircial <m15957166524@163.com>
2021-12-15 00:13:57 +0800

    update test

--- test_agent.py
+++ test_agent.py
@@ -2,7 +2,7 @@ from ic.agent import *
 from ic.identity import *
 from ic.client import *
 # from ic.candid import Types, encode
-from ic.candid2 import Interface_IDL as idl, encode
+from ic.candid2 import Interface_IDL as Types, encode
 
 client = Client()
 iden = Identity(privkey="833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42")
@@ -18,7 +18,7 @@ ret = ag.query_raw(
         "gvbup-jyaaa-aaaah-qcdwa-cai",
         "balanceOf",
         encode([
-            {'type': idl.Principal, 'value': iden.sender().bytes}
+            {'type': Types.Principal, 'value': iden.sender().bytes}
         ])
       )
 print(ret)
@@ -28,8 +28,8 @@ ret = ag.update_raw(
         "gvbup-jyaaa-aaaah-qcdwa-cai",
         "transfer",
         encode([
-            {'type': idl.Principal, 'value': 'aaaaa-aa'},
-            {'type': idl.Nat, 'value': 10000000000}
+            {'type': Types.Principal, 'value': 'aaaaa-aa'},
+            {'type': Types.Nat, 'value': 10000000000}
             ])
         )
 print(ret)
