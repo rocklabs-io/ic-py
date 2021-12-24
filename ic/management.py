2475f9a6a1e6abec9f8d9634993385e694c0de20
ccyanxyz <ccyanxyz@gmail.com>
2021-12-24 16:38:33 +0800

    add aaaaa-aa

---
+++ ic/management.py
@@ -0,0 +1,7 @@
+
+# management canister interface `aaaaa-aa`
+# wrap basic interfaces provided by the management canister:
+# create_canister, install_code, canister_status, etc.
+
+class Management:
+    pass
