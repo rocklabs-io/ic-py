41ca87eccf32129a87092a11238f6b45a82897ef
Yvon <602149025@qq.com>
2022-06-23 13:47:08 +0800

    release 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), 'r') as f:
 
 setup(
     name = 'ic-py',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'Python Agent Library for the Internet Computer',
     long_description = long_description,
     long_description_content_type = "text/markdown",
